56ef0fb0cc991766c09501ef2d96175f74a576c1
alejandroq <quesadar@dukes.jmu.edu>
2016-04-02 05:06:32 -0400

    HelloWorld

---
+++ footer.php
@@ -0,0 +1,17 @@
+		<!-- FOOTER -->
+	    <footer>
+	    	<!-- add content here -->
+	    </footer>
+	    <!-- END FOOTER -->
+
+	    <!-- SCRIPTS -->
+	    	<!-- JQUERY -->
+		    <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+			<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+			<!-- PLUGINS.JS -->
+			<script src="js/plugins.js"></script>
+			<!-- MAIN.JS -->
+			<script src="js/main.js"></script>
+		<!-- END SCRIPTS -->
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html class="no-js" lang="">
+	<head>
+	    <title>coffee</title>
+	    <meta charset="utf-8"/>
+	    <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+	    <meta name="description" content="Drinking Coffee since 1993."/>
+	    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+	    <!-- FAVICON (include in Root directory) -->
+	    <link rel="shortcut icon" type="image/png" href="favicon.png"/>
+	    <!-- END FAVICON -->
+
+	    <!-- RALEWAY FONT -->
+	    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css"/>
+	    <!-- END RALEWAY FONT -->
+
+	   	<!-- STYLESHEETS -->
+	    <link rel="stylesheet" type="text/css" href="css/normalize.min.css" />
+	    <link rel="stylesheet" type="text/css" href="css/jquery-ui.min.css" />
+	    <!-- Latest compiled and minified CSS -->
+	    <link rel="stylesheet/less" type="text/css" href="css/main.less" />
+	    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+	    <!-- END STYLESHEETS -->
+
+	    <!-- JAVASCRIPT -->
+	    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+	    <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
+	    <!-- END JAVASCRIPT -->
+	</head>
+	<body>
+		<!-- HEADER -->
+		<header>
+			<h1>Hello World!</h1>
+		</header>
+		<!-- END HEADER -->
