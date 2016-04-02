39181deb4853aca8e5355d56c97e67080c89dca9
jtm428 <jonathantmicucci@gmail.com>
2016-04-02 16:09:12 -0400

    Update main.js

--- htdoc/js/main.js
+++ htdoc/js/main.js
@@ -4,4 +4,65 @@ $(document).ready(function() {
         // put your options and callbacks here
     })
 
+    /* Set all views to 'none' */
+    $('#notepad_view').css({
+        'display': 'none'
+    });
+    $('#calendar_view').css({
+        'display': 'none'
+    });
+    $('#reminder_view').css({
+        'display': 'none'
+    });
+    $('#file_manager_view').css({
+        'display': 'none'
+    });
+});
+
+/* Close and open of notepad */
+$('#notepad_close').click(function() {
+    $('#notepad_view').css({
+        'display': 'none'
+    });
+});
+$('#notepad_open').click(function() {
+    $('#notepad_view').css({
+        'display': 'block'
+    });
+});
+
+/* Close and open of calendar */
+$('#calendar_close').click(function() {
+    $('#calendar_view').css({
+        'display': 'none'
+    });
+});
+$('#calendar_open').click(function() {
+    $('#calendar_view').css({
+        'display': 'block'
+    });
+});
+
+/* Close and open of reminder */
+$('#reminder_close').click(function() {
+    $('#reminder_view').css({
+        'display': 'none'
+    });
+});
+$('#reminder_open').click(function() {
+    $('#reminder_view').css({
+        'display': 'block'
+    });
+});
+
+/* Close and open of file mang. */
+$('#file_close').click(function() {
+    $('#file_manager_view').css({
+        'display': 'none'
+    });
+});
+$('#file_open').click(function() {
+    $('#file_manager_view').css({
+        'display': 'block'
+    });
 });
