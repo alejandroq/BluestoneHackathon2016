04672868592a6fee6c9353c2cb8038d3cbbb8ad2
alejandroq <quesadar@dukes.jmu.edu>
2016-04-02 12:30:39 -0400

    new stuff

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php 
 	include 'header.php';
-
 	include 'functions.php';
-
-	include 'footer.php';
 ?>
+	<article class="row">
+		<h1 class="col-md-4">Hello World!</h1>
+	</article>
+
+<?php include 'footer.php'; ?>
