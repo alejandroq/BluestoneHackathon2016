a7f17d2cd5d3de7e7fcdde7964e362cc60708c13
Alejandro Quesada <alejandroq@users.noreply.github.com>
2016-04-02 17:39:56 -0400

    Update main.js

--- htdoc/js/main.js
+++ htdoc/js/main.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
     })
 
     /* Set all views to 'none' */
-    // $('#notepad_view').css({
-    //     'display': 'none'
-    // });
+    $('#notepad_view').css({
+        'display': 'none'
+    });
     // $('#calendar_view').css({
     //     'display': 'none'
     // });
@@ -17,60 +17,44 @@ $(document).ready(function() {
     $('#file_manager_view').css({
         'display': 'none'
     });
-    $('#admin_view').css({
+    $('#chat_view').css({
         'display': 'none'
     });
-    $('#chat_view').css({
+    $('#admin_view').css({
         'display': 'none'
     });
 });
 
 /* Close and open of notepad */
 $('#notepad_close').click(function() {
-    $('#notepad_view').css({
-        'display': 'none'
-    });
+    $('#notepad_view').toggle();
 });
 $('#notepad_open').click(function() {
-    $('#notepad_view').css({
-        'display': 'block'
-    });
+    $('#notepad_view').toggle();
 });
 
 /* Close and open of calendar */
 $('#calendar_close').click(function() {
-    $('#calendar_view').css({
-        'display': 'none'
-    });
+    $('#calendar_view').toggle();
 });
 $('#calendar_open').click(function() {
-    $('#calendar_view').css({
-        'display': 'block'
-    });
+    $('#calendar_view').toggle();
 });
 
 /* Close and open of reminder */
 $('#reminder_close').click(function() {
-    $('#reminder_view').css({
-        'display': 'none'
-    });
+    $('#reminder_view').toggle();
 });
 $('#reminder_open').click(function() {
-    $('#reminder_view').css({
-        'display': 'block'
-    });
+    $('#reminder_view').toggle();
 });
 
 /* Close and open of file mang. */
 $('#file_close').click(function() {
-    $('#file_manager_view').css({
-        'display': 'none'
-    });
+    $('#file_manager_view').toggle();
 });
 $('#file_open').click(function() {
-    $('#file_manager_view').css({
-        'display': 'block'
-    });
+    $('#file_manager_view').toggle();
 });
 $('#settings').click(function() {
 	$('#list_view').toggle().empty().append('<a href="logout.php"><li><i class="fa fa-sign-out"></i>&nbsp;Logout</li></a>');
@@ -81,26 +65,16 @@ $('#events').click(function() {
 });
 /* Close and open of file mang. */
 $('#chat_close').click(function() {
-    $('#chat_view').css({
-        'display': 'none'
-    });
+    $('#chat_view').toggle();
 });
 $('#chat_open').click(function() {
-    $('#chat_view').css({
-        'display': 'block'
-    });
+    $('#chat_view').toggle();
 });
-
-/* Close and open of file mang. */
 $('#admin_close').click(function() {
-    $('#admin_view').css({
-        'display': 'none'
-    });
+    $('#admin_view').toggle();
 });
 $('#admin_open').click(function() {
-    $('#admin_view').css({
-        'display': 'block'
-    });
+    $('#admin_view').toggle();
 });
 
 function grabNewEvents() {
@@ -121,3 +95,4 @@ function grabNewEvents() {
     //PERFORM REQUEST
   $.ajax(options);
 } //END OF FORM SUBMISSION
+
