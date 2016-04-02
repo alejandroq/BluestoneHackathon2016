167dd5e8a40a76ca262f950dc4f7e68d17839608
jtm428 <jonathantmicucci@gmail.com>
2016-04-02 16:08:56 -0400

    Update footer.php

--- htdoc/footer.php
+++ htdoc/footer.php
@@ -1,6 +1,114 @@
 		<!-- FOOTER -->
 	    <footer>
-	    	<div class="row" style="text-align: center; color:gray;"><p>Website & Content &copy; 2016 Folger People</p></div>
+			<nav class="navbar navbar-default navbar-fixed-bottom">
+	    		<div class = "row footer_bar">
+	    			<div class="col-md-3">
+						<div class="btn-group dropup function_menu">
+						  	<button type="button" class="btn btn-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						    	Functions   
+						    	<span class="caret"></span>
+						    	<span class="sr-only">Toggle Dropdown</span>
+						  	</button>
+						  	<ul class="dropdown-menu functions_in_menu">
+								<table class="table table-hover">
+									<tr id = "calendar_open">
+										<td>Calendar</td>
+										<td>   </td>
+									</tr>
+									<tr id = "reminder_open">
+										<td>Reminders</td>
+										<td>   </td>
+									</tr>
+									<tr id = "notepad_open">
+										<td>Notepad</td>
+										<td>   </td>
+									</tr>
+									<tr id = "file_open">
+										<td>File Manager</td>
+										<td>   </td>
+									</tr>
+								</table>
+						  	</ul>
+						</div>
+	    			</div>
+
+	    			<div class="col-md-1"></div>
+
+	    			<div class="col-md-4">
+	    				<!--<p id="words">Website and ontent &copy; 2016 CoffeeTeamBravo. March 31st, 2016</p>-->
+	    			</div>
+
+	    			<div class="col-md-1"></div>
+
+	    			<div class="col-md-3">
+						<div class="btn-group dropup work_status_bar">
+						  	<button type="button" class="btn btn-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						    	Online&nbsp;
+						    	<span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span>
+						    	<span class="sr-only">Toggle Dropdown</span>
+						  	</button>
+						  	<ul class="dropdown-menu">
+								<table class="table table-hover">
+									<tr>
+										<td>J. Smith</td>
+										<td>   
+											<span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span>
+										</td>
+									</tr>
+									<tr>
+										<td>T Karren.</td>
+										<td>   
+											<span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span>
+										</td>
+									</tr>
+									<tr>
+										<td>B. Paul</td>
+										<td>  
+											<span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span> 
+										</td>
+									</tr>
+									<tr>
+										<td>W. Wallis</td>
+										<td>  
+											<span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span>
+										</td>
+									</tr>
+									<tr>
+										<td>M. Lloyd</td>
+										<td>   
+											<span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span>
+										</td>
+									</tr>
+									<tr>
+										<td>P. Low</td>
+										<td>   
+											<span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span>
+										</td>
+									</tr>
+									<tr>
+										<td>F. Peer</td>
+										<td>   
+											<span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span>
+										</td>
+									</tr>
+									<tr>
+										<td>K. Yols</td>
+										<td>   
+											<span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span>
+										</td>
+									</tr>
+									<tr>
+										<td>R. Loke</td>
+										<td>   
+											<span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span>
+										</td>
+									</tr>
+								</table>
+						  	</ul>
+						</div>
+	    			</div>
+	    		</div>	
+			</nav>
 	    </footer>
 	    <!-- END FOOTER -->
 
@@ -23,6 +131,7 @@
 			<script src='css/calendar/lib/jquery.min.js'></script>
 			<script src='css/calendar/lib/moment.min.js'></script>
 			<script src='css/calendar/fullcalendar.js'></script>
+	    	<link rel="stylesheet/less" type="text/css" href="css/main.less" />
 			<!-- END CALENDAR -->
 			<!-- END SCRIPTS -->
 	</body>
