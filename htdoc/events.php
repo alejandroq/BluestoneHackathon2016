2cb7a642986e8975803c94ffb0bf93bd35f3d7c0
Alejandro Quesada <alejandroq@users.noreply.github.com>
2016-04-02 17:42:02 -0400

    Update events.php

--- htdoc/events.php
+++ htdoc/events.php
@@ -2,7 +2,7 @@
 
 include '../connection.php';
 
-$sql = "SELECT * FROM Event WHERE event_Begins >= CURDATE() ORDER BY event_Begins ASC";
+$sql = "SELECT * FROM Event WHERE event_Begins >= CURDATE() ORDER BY event_Begins ASC LIMIT 3";
 $result = $mysqli->query($sql);
 if ($result->num_rows > 0)
 {
@@ -10,7 +10,7 @@ if ($result->num_rows > 0)
 	$i = 0;
 	while ($rows = $result->fetch_assoc())
 	{
-		$return .= '<li>' . $rows['event_EventName'] . ' begins at ' . $rows['event_Begins'] . '</li>';
+		$return .= '<li><i class="fa fa-calendar"></i>&nbsp;' . $rows['event_EventName'] . ' begins at ' . $rows['event_Begins'] . '</li>';
 	}
 	return print_r($return);
 }
