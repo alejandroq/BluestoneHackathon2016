694d0123a28014c534da4db362a9cdc3a23bdf81
alejandroq <quesadar@dukes.jmu.edu>
2016-04-02 10:25:01 -0400

    Bootstrap lol

--- header.php
+++ header.php
@@ -22,11 +22,13 @@
 	    <!-- Latest compiled and minified CSS -->
 	    <link rel="stylesheet/less" type="text/css" href="css/main.less" />
 	    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	    <!-- END STYLESHEETS -->
 
 	    <!-- JAVASCRIPT -->
 	    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
 	    <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
+	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	    <!-- END JAVASCRIPT -->
 	</head>
 	<body>
